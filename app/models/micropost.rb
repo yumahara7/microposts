0ac39f262d0dc8e128f848a266ac187153862d53
yumahara7 <harayuma0720@gmail.com>
2016-03-30 14:09:41 +0000

    delete-paginate_per

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -2,7 +2,7 @@ class Micropost < ActiveRecord::Base
    belongs_to :user
   validates :user_id, presence: true
   validates :content, presence: true, length: { maximum: 140 }
-  paginates_per 6
+
 
   def trueuser
     if reposted_user_id.present?
